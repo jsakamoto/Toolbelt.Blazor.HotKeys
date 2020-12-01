e0d10550ef393618899f025327c2f4e078e3c065
jsakamoto <junichi.jp@gmail.com>
2020-12-01 17:34:05 +0900

    Trying to server-side support.

--- Toolbelt.Blazor.HotKeys/script.js
+++ Toolbelt.Blazor.HotKeys/script.js
@@ -6,16 +6,23 @@ var Toolbelt;
         var HotKeys;
         (function (HotKeys) {
             var HotkeyEntry = (function () {
-                function HotkeyEntry(hotKeyEntryWrpper) {
+                function HotkeyEntry(hotKeyEntryWrpper, modKeys, key, allowIn) {
                     this.hotKeyEntryWrpper = hotKeyEntryWrpper;
+                    this.modKeys = modKeys;
+                    this.key = key;
+                    this.allowIn = allowIn;
                 }
+                HotkeyEntry.prototype.action = function () {
+                    this.hotKeyEntryWrpper.invokeMethodAsync('Action');
+                };
                 return HotkeyEntry;
             }());
+            var NonTextInputTypes = ["button", "checkbox", "color", "file", "image", "radio", "range", "reset", "submit",];
             var idSeq = 0;
             var hotKeyEntries = {};
-            function register(hotKeyEntryWrpper) {
+            function register(hotKeyEntryWrpper, modKeys, key, allowIn) {
                 var id = idSeq++;
-                var hotKeyEntry = new HotkeyEntry(hotKeyEntryWrpper);
+                var hotKeyEntry = new HotkeyEntry(hotKeyEntryWrpper, modKeys, key, allowIn);
                 hotKeyEntries[idSeq] = hotKeyEntry;
                 return id;
             }
@@ -24,21 +31,51 @@ var Toolbelt;
                 delete hotKeyEntries[id];
             }
             HotKeys.unregister = unregister;
-            function onKeyDown(modKeys, keyCode, tagName, type) {
+            function onKeyDown(e) {
+                var preventDefault = false;
                 for (var key in hotKeyEntries) {
                     if (!hotKeyEntries.hasOwnProperty(key))
                         continue;
                     var entry = hotKeyEntries[key];
+                    if (entry.key !== e.keyCode)
+                        continue;
+                    var modKeys = entry.modKeys;
+                    if (entry.key == 16)
+                        modKeys |= 1;
+                    if (entry.key == 17)
+                        modKeys |= 2;
+                    if (entry.key == 18)
+                        modKeys |= 4;
+                    if (e.modKeys != modKeys)
+                        continue;
+                    if (!isAllowedIn(entry, e.tagName, e.type))
+                        continue;
+                    preventDefault = true;
+                    entry.action();
                 }
+                return preventDefault;
             }
-            function attach(hotKeysWrpper) {
+            function isAllowedIn(entry, tagName, type) {
+                if (tagName === "TEXTAREA") {
+                    return (entry.allowIn & 2) === 2;
+                }
+                if (tagName == "INPUT") {
+                    if ((type !== null) &&
+                        (NonTextInputTypes.indexOf(type) !== -1) &&
+                        (entry.allowIn & 4) === 4)
+                        return true;
+                    return (entry.allowIn & 1) === 1;
+                }
+                return true;
+            }
+            function attach() {
                 document.addEventListener('keydown', function (ev) {
+                    var modKeys = (ev.shiftKey ? 1 : 0) +
+                        (ev.ctrlKey ? 2 : 0) +
+                        (ev.altKey ? 4 : 0) +
+                        (ev.metaKey ? 8 : 0);
                     var keyCode = ev.keyCode;
-                    var modKeys = (ev.shiftKey ? 0x01 : 0) +
-                        (ev.ctrlKey ? 0x02 : 0) +
-                        (ev.altKey ? 0x04 : 0) +
-                        (ev.metaKey ? 0x08 : 0);
-                    var preventDefault = hotKeysWrpper.invokeMethod('OnKeyDown', modKeys, keyCode, ev.srcElement.tagName, ev.srcElement.getAttribute('type'));
+                    var preventDefault = onKeyDown({ modKeys: modKeys, keyCode: keyCode, tagName: ev.srcElement.tagName, type: ev.srcElement.getAttribute('type') });
                     if (preventDefault)
                         ev.preventDefault();
                 });
--- Toolbelt.Blazor.HotKeys/script.ts
+++ Toolbelt.Blazor.HotKeys/script.ts
@@ -1,18 +1,48 @@
 ﻿namespace Toolbelt.Blazor.HotKeys {
 
+    const enum AllowIn {
+        None = 0b0000,
+        Input = 0b0001,
+        TextArea = 0b0010,
+        NonTextInput = 0b0100,
+    }
+
+    const enum ModKeys {
+        None = 0,
+        Shift = 0x01,
+        Ctrl = 0x02,
+        Alt = 0x04,
+        Meta = 0x08
+    }
+
+    const enum Keys {
+        Shift = 0x10,
+        Ctrl = 0x11,
+        Alt = 0x12,
+    }
+
     class HotkeyEntry {
         constructor(
-            private hotKeyEntryWrpper: any
+            private hotKeyEntryWrpper: any,
+            public modKeys: ModKeys,
+            public key: number,
+            public allowIn: AllowIn
         ) {
         }
+
+        public action(): void {
+            this.hotKeyEntryWrpper.invokeMethodAsync('Action');
+        }
     }
+
+    const NonTextInputTypes = ["button", "checkbox", "color", "file", "image", "radio", "range", "reset", "submit",];
 
     let idSeq: number = 0;
     const hotKeyEntries: { [key: number]: HotkeyEntry } = {};
 
-    export function register(hotKeyEntryWrpper: any): number {
+    export function register(hotKeyEntryWrpper: any, modKeys: ModKeys, key: number, allowIn: AllowIn): number {
         const id = idSeq++;
-        const hotKeyEntry = new HotkeyEntry(hotKeyEntryWrpper);
+        const hotKeyEntry = new HotkeyEntry(hotKeyEntryWrpper, modKeys, key, allowIn);
         hotKeyEntries[idSeq] = hotKeyEntry;
         return id;
     }
@@ -21,25 +51,61 @@
         delete hotKeyEntries[id];
     }
 
-    function onKeyDown(modKeys: number, keyCode: number, tagName: string, type: string | null): void {
+    function onKeyDown(e: { modKeys: ModKeys, keyCode: number, tagName: string, type: string | null }): boolean {
+
+        let preventDefault = false;
 
         for (const key in hotKeyEntries) {
             if (!hotKeyEntries.hasOwnProperty(key)) continue;
             const entry = hotKeyEntries[key];
-            //entry.
+
+            if (entry.key !== e.keyCode) continue;
+
+            let modKeys = entry.modKeys;
+            if (entry.key == Keys.Shift) modKeys |= ModKeys.Shift;
+            if (entry.key == Keys.Ctrl) modKeys |= ModKeys.Ctrl;
+            if (entry.key == Keys.Alt) modKeys |= ModKeys.Alt;
+            if (e.modKeys != modKeys) continue;
+
+            if (!isAllowedIn(entry, e.tagName, e.type)) continue;
+
+            preventDefault = true;
+
+            entry.action();
         }
+
+        return preventDefault;
     }
 
-    export function attach(hotKeysWrpper: any): void {
+    function isAllowedIn(entry: HotkeyEntry, tagName: string, type: string | null): boolean {
+        if (tagName === "TEXTAREA") {
+            return (entry.allowIn & AllowIn.TextArea) === AllowIn.TextArea;
+        }
+
+        if (tagName == "INPUT") {
+            if ((type !== null) &&
+                (NonTextInputTypes.indexOf(type) !== -1) &&
+                (entry.allowIn & AllowIn.NonTextInput) === AllowIn.NonTextInput
+            ) return true;
+
+            return (entry.allowIn & AllowIn.Input) === AllowIn.Input;
+        }
+
+        return true;
+    }
+
+    export function attach(/*hotKeysWrpper: any*/): void {
 
         document.addEventListener('keydown', ev => {
+            const modKeys =
+                (ev.shiftKey ? ModKeys.Shift : 0) +
+                (ev.ctrlKey ? ModKeys.Ctrl : 0) +
+                (ev.altKey ? ModKeys.Alt : 0) +
+                (ev.metaKey ? ModKeys.Meta : 0);
             const keyCode = ev.keyCode;
-            let modKeys =
-                (ev.shiftKey ? 0x01 : 0) +
-                (ev.ctrlKey ? 0x02 : 0) +
-                (ev.altKey ? 0x04 : 0) +
-                (ev.metaKey ? 0x08 : 0);
-            let preventDefault = hotKeysWrpper.invokeMethod('OnKeyDown', modKeys, keyCode, (ev.srcElement as HTMLElement).tagName, (ev.srcElement as HTMLElement).getAttribute('type')) as boolean;
+            const preventDefault =
+                onKeyDown({ modKeys, keyCode, tagName: (ev.srcElement as HTMLElement).tagName, type: (ev.srcElement as HTMLElement).getAttribute('type') });
+            //hotKeysWrpper.invokeMethod('OnKeyDown', modKeys, keyCode, (ev.srcElement as HTMLElement).tagName, (ev.srcElement as HTMLElement).getAttribute('type')) as boolean;
             if (preventDefault) ev.preventDefault();
         });
     }
