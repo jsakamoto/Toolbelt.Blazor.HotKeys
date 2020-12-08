d7126858246d9b97a444b5f99b9c955671ca6796
jsakamoto <junichi.jp@gmail.com>
2020-12-08 17:51:41 +0900

    Add the sample site built on Blazor Server.

---
+++ SampleSites/Server/Data/WeatherForecastService.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using SampleSite.Components.Services;
+
+namespace SampleSite.Server.Data
+{
+    public class WeatherForecastService : IWeatherForecastService
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public Task<WeatherForecast[]> GetForecastAsync()
+        {
+            var rng = new Random();
+            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Today.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            }).ToArray());
+        }
+    }
+}
