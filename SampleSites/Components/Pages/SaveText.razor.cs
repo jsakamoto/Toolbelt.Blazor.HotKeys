23d292d4fe4206a7870b077198855a273cc6a871
jsakamoto <junichi.jp@gmail.com>
2021-07-22 21:23:52 +0900

    sample code: Implement demonstration for saving text by Ctrl+S

---
+++ SampleSites/Components/Pages/SaveText.razor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using Toolbelt.Blazor.HotKeys;
+
+namespace SampleSite.Components.Pages
+{
+    public partial class SaveText : IDisposable
+    {
+        [Inject] public HotKeys HotKeys { get; set; }
+
+        [Inject] public IJSRuntime JS { get; set; }
+
+        private HotKeysContext HotKeysContext;
+
+        private ElementReference InputElement;
+
+        private string InpuText = "";
+
+        private readonly List<string> SavedTexts = new List<string>();
+
+        protected override void OnInitialized()
+        {
+            this.HotKeysContext = this.HotKeys.CreateContext()
+                .Add(ModKeys.Ctrl, Keys.S, () => this.OnSaveText(), allowIn: AllowIn.Input);
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+            if (firstRender)
+            {
+#if ENABLE_FOCUS
+                await this.InputElement.FocusAsync();
+#endif
+            }
+        }
+
+        private async Task OnSaveText()
+        {
+            await this.JS.InvokeVoidAsync("fireOnChange", this.InputElement);
+
+            this.SavedTexts.Add(this.InpuText);
+            this.StateHasChanged();
+        }
+
+        public void Dispose()
+        {
+            this.HotKeysContext?.Dispose();
+        }
+    }
+}
---
+++ SampleSites/Components/wwwroot/helper.js
@@ -0,0 +1,5 @@
+﻿function fireOnChange(element) {
+    var event = new Event('change');
+    element.dispatchEvent(event);
+    console.log('onchange has been fired.', element)
+}
