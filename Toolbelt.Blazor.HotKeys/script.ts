067497f9d6e52ab5c69ed664577224a39607afa3
jsakamoto <junichi.jp@gmail.com>
2021-08-08 20:38:14 +0900

    Fix: finding the search path sometimes fails.

--- Toolbelt.Blazor.HotKeys/script.ts
+++ Toolbelt.Blazor.HotKeys/script.ts
@@ -1,5 +1,5 @@
 ﻿namespace Toolbelt.Blazor.HotKeys {
-    const searchParam = document.currentScript?.getAttribute('src')?.split('?').pop() || '';
+    const searchParam = document.currentScript?.getAttribute('src')?.split('?')[1] || '';
     export var ready = import('./script.module.min.js?' + searchParam).then(m => {
         Object.assign(HotKeys, m.Toolbelt.Blazor.HotKeys);
     });
