dbe50e5b67e72e7927a552d0b931d4afdcc2c958
jsakamoto <jsakamoto@users.noreply.github.com>
2023-01-22 06:12:29 +0000

    deploy: cf93df9710941f4471eac315a332d54eb39ef162

---
+++ _content/SampleSite.Components/helper.js
@@ -0,0 +1,21 @@
+﻿var Toolbelt;
+(function (Toolbelt) {
+    var Blazor;
+    (function (Blazor) {
+        Blazor.fireOnChange = (element) => {
+            const event = new Event('change');
+            element.dispatchEvent(event);
+            console.log('onchange has been fired.', element)
+        }
+
+        Blazor.fireOnKeyDown = (args) => {
+            const element = document.querySelector(args.selector);
+            const event = new KeyboardEvent("keydown", { ...args.options, ...{ bubbles: true } });
+            element.dispatchEvent(event);
+        }
+
+        Blazor.log = (text) => console.log(text);
+
+    })(Blazor = Toolbelt.Blazor || (Toolbelt.Blazor = {}));
+})(Toolbelt || (Toolbelt = {}));
+
